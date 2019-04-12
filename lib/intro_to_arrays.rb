eda3d56eab5d991251146d2200d9d3edd9c8f6e5
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-12 04:02:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,36 @@
 def instantiate_new_array
   array = []
 end
+
+def array_with_two_elements
+  Array.new(2)
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element (array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  Array.first 
+end
+
+def last_element_with_array_methods (array)
+  Array.last
+end
+
+def length_of_array (array)
+  Array.length(array)
+end
+
+
+
+
+  
